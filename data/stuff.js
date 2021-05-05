c4e1db5f1c4895719eb9042f873dd12ac4642d52
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-05-05 08:37:49 -0700

    create data and adjust files to match data

---
+++ data/stuff.js
@@ -0,0 +1,104 @@
+// example seed data
+const stuff = [
+  {
+    id: expect.any(Number),
+    name: 'lamp',
+    type: 'furniture',
+    description: 'A wooden desk lamp.',
+    isSentimental: false,
+    yearAcquired: 2020,
+    color: 'brown'
+  },
+  {
+    id: expect.any(Number),
+    name: 'radio',
+    type: 'furniture',
+    description: 'A tabletop FM radio.',
+    isSentimental: false,
+    yearAcquired: 2021,
+    color: 'brown'
+  },
+  {
+    id: expect.any(Number),
+    name: 'bouquet',
+    type: 'plant',
+    description: 'Some flowers I picked out in the city, in a jar.',
+    isSentimental: true,
+    yearAcquired: 2021,
+    color: 'green'
+  },
+  {
+    id: expect.any(Number),
+    name: 'books',
+    type: 'things',
+    description: 'A few stacks of books, probably around 100.',
+    isSentimental: true,
+    yearAcquired: 2014,
+    color: 'green'
+  },
+  {
+    id: expect.any(Number),
+    name: 'poem',
+    type: 'art',
+    description: 'A Marilyn Hacker poem transcribed on handmade paper that I got from my partner as a gift.',
+    isSentimental: true,
+    yearAcquired: 2020,
+    color: 'blue'
+  },
+  {
+    id: expect.any(Number),
+    name: 'rug',
+    type: 'textiles',
+    description: 'A flatwoven rug from Ikea.',
+    isSentimental: false,
+    yearAcquired: 2021,
+    color: 'white'
+  },
+  {
+    id: expect.any(Number),
+    name: 'blanket',
+    type: 'textiles',
+    description: 'A wool blanket hanging on my door.',
+    isSentimental: false,
+    yearAcquired: 2021,
+    color: 'blue'
+  },
+  {
+    id: expect.any(Number),
+    name: 'curtain',
+    type: 'textiles',
+    description: 'Natural color linen curtain with wooden curtain rings.',
+    isSentimental: false,
+    yearAcquired: 2020,
+    color: 'white'
+  },
+  {
+    id: expect.any(Number),
+    name: 'desk',
+    type: 'furniture',
+    description: 'An old hardwood desk, pretty small. It has three drawers',
+    isSentimental: false,
+    yearAcquired: 2020,
+    color: 'brown'
+  },
+  {
+    id: expect.any(Number),
+    name: 'yoga mat',
+    type: 'things',
+    description: 'A leaf green yoga mat.',
+    isSentimental: true,
+    yearAcquired: 2018,
+    color: 'green'
+  },
+  {
+    id: expect.any(Number),
+    name: 'computer',
+    type: 'things',
+    description: 'A space gray Macbook Pro.',
+    isSentimental: true,
+    yearAcquired: 2021,
+    color: 'gray'
+  }
+];
+
+export default stuff;
